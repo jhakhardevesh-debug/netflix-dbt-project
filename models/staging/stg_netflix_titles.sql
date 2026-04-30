50694ef0c4ab2d429e12c24f74373409bfee8bbb
jhakhardevesh@gmail.com <jhakhardevesh@gmail.com>
2026-04-30 17:27:22 +0000

    same again

--- models/staging/stg_netflix_titles.sql
+++ models/staging/stg_netflix_titles.sql
@@ -1,3 +1,14 @@
-{{ config(materialized='table', schema='ANALYTICS') }}
-
-select 1 as id
+select
+ show_id,
+ type,
+ title,
+ director,
+ 'cast' as cast_members,
+ country,
+ date_added,
+ release_year,
+ rating,
+ duration,
+ listed_in,
+ description
+from {{ source('netflix_raw', 'NETFLIX_TITLES') }}
